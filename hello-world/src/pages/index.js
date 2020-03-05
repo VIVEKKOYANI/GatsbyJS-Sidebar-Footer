f2f257ff7a457c0478832ebdc5572ddc219bb449
vivek <vivek.koyani@gmail.com>
2020-03-05 23:51:41 +0530

    Link add

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,9 +1,14 @@
 import React from "react"
 import Header from '../components/header'
-
+import {Link} from 'gatsby'
 export default () =>( 
     
 <div style={{color: `purple`, fontSize: `72px`}}>
+    <Link  to="/profile" >Profile</Link>
+    <Link  to="/product" >Products</Link>
+    <Link  to="/contact" >Contact</Link>
+    <Link  to="/enquiry" >Enquiry</Link>
+    <Link  to="/quality" >Quality</Link>
     <Header  headerText="Hello Gatsby" />
    <h3> Hello world!</h3>
     </div>
