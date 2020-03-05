c0c8d465b08db260f28613f8be6a3056bbe1b1a5
vivek <vivek.koyani@gmail.com>
2020-03-05 23:46:24 +0530

    props pass

--- hello-world/src/components/header.js
+++ hello-world/src/components/header.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default () => (
     <div>
-        <h1>This is a header</h1>
+        <h1>{props.headerText}</h1>
     </div>
 )
--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -4,7 +4,7 @@ import Header from '../components/header'
 export default () =>( 
     
 <div style={{color: `purple`, fontSize: `72px`}}>
-    <Header />
+    <Header  headerText="Hello Gatsby" />
    <h3> Hello world!</h3>
     </div>
 )
