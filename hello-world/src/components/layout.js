57ffce225e0c758fbd39d93ad8976cfbc513bfc8
vivek <vivek.koyani@gmail.com>
2020-03-07 23:51:57 +0530

    add layout in link

--- hello-world/src/components/layout.js
+++ hello-world/src/components/layout.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Link } from 'gatsby'
+
+const ListLink = props => (
+    <li style={{display: `inline-block`, marginRight: `1rem`}}>
+        <Link to={props.to}>{props.children}</Link>
+    </li>
+)
 
 export default ({children}) => (
     <div style={{margin: `3rem auto`, maxWidth: 650, padding: `0 1rem`}}>
-        <h3>MySite</h3>
+        <header style={{ marginBottom: `1.5rem`}} >
+            <Link to="/" style={{ textShadow: `none`, backgroundImage: `none`}}>
+        <h3 style={{ display: `inline`}}>MySite</h3>
+        </Link>
+        <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/profile/">Profile</ListLink>
+        <ListLink to="/product/">Products</ListLink>
+        <ListLink to="/contact/">Contact</ListLink>
+        <ListLink to="/enquiry/">Enquiry</ListLink>
+        <ListLink to="/quality/">Quality</ListLink>
+      </ul>
         {children}
+        </header>
     </div>
 )
